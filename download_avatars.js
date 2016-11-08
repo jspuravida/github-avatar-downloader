6e7cf079085d8148fc0311f7a85a28ec7b1ece59
jspuravida <julian.salari@gmail.com>
2016-11-08 02:13:46 +0000

    Added repo contributor function, callback, error function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,20 @@
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+function getRepoContributors(repoOwner, repoName, cb) {
+  request.get("http://api.github.com/repos/jquery/jquery/contributors")
+  .on('error', function (err) {          // Will show us the error if present
+    console.log(err.stack);
+  })
+  .on('response', function (response) {   // Will display the status code if po
+         console.log('Response Status Code: ', response.statusCode);
+         console.log('Response Status Message: ', response.statusMessage);
+  })
+
+}
+
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});

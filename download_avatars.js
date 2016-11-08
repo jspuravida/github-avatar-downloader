5134952fea178c7a6fb7c3693b395d4ed9e21d48
jspuravida <julian.salari@gmail.com>
2016-11-08 03:44:58 +0000

    Added the request and User Agent variable

--- download_avatars.js
+++ download_avatars.js
@@ -7,9 +7,19 @@ var GITHUB_TOKEN = "76808c34b4d57eeb6374889281e9109074613845";
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
-console.log(requestURL);
+  var requestOptions = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(requestOptions, function(err, response, body) {
+    if (err) throw err;
+    console.log('Response Status Code:', response.body);
+  });
 }
 
 

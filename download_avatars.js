a003e1eee6f6aa7dfd13ea71d607eea329200d20
jspuravida <julian.salari@gmail.com>
2016-11-08 02:50:17 +0000

    Added step 4 of exercise. Created the requestURL, github user, token.

--- download_avatars.js
+++ download_avatars.js
@@ -1,13 +1,15 @@
 var request = require('request');
 
-// var GITHUB_USER = "jspuravida";
-// var GITHUB_TOKEN = "76808c34b4d57eeb6374889281e9109074613845";
+var GITHUB_USER = "jspuravida";
+var GITHUB_TOKEN = "76808c34b4d57eeb6374889281e9109074613845";
 
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
+console.log(requestURL);
 }
 
 
